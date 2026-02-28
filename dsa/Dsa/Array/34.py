4600be17acd42b1c6c48c4dda0b0924d5d997671
Swaraj Sahay Verma <0157al231210@lnct.ac.in>
2026-02-28 10:05:52 +0530

    28/2/26

---
+++ dsa/Dsa/Array/34.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.searchBin(nums, target, True)
+        right = self.searchBin(nums, target, False)
+        return [left,right]
+
+    
+    def searchBin(self, nums, target, leftbias):
+        l,r = 0, len(nums) - 1
+        i = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                i = mid
+                if leftbias:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return i
