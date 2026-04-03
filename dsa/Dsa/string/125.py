ac95406afbe38cc5d080491b122c69384ff7466c
Swaraj Sahay Verma <0157al231210@lnct.ac.in>
2026-04-03 08:56:02 +0530

    3/4/26

--- dsa/Dsa/string/125.py
+++ dsa/Dsa/string/125.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         right = len(s) - 1
         while left < right:
-            if not s[left].isolnum():
+            if not s[left].isalnum():
                 left += 1
             elif not s[right].isalnum():
                 right -= 1
